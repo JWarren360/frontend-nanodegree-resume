6421ed1c0be31fc198126739b42bb87a9589e339
John Warren <JWarren360@gmail.com>
2015-09-01 21:26:03 -0500

    Builing JSON Structure

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,125 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio = {
+	"name" : "John Warren",
+	"role" : "Front-End Web Developer",
+	"contacts" : {
+		"email" : "JWarren360@gmail.com",
+		"mobile" : "512-739-3897",
+		"github" : "JWarren360";
+		"location" : "Austin , TX"
+	},
+	"welcomeMessage" : "Welcome to my awesome resume!",
+	"skill" : [
+		"html",
+		"javascript",
+		"Awesomeness"
+	],
+	"pictureURL" : "images/fry.jpg"
+}
+var education = {
+	"school" : [
+		{
+			"name" : "Austin Comunity College",
+			"year" : 2,
+			"major" : "Eletronic Repair"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Front-End Web Development",
+			"School" : "Udacity",
+			"date" : 2015
+		}
+	]
+}
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Four Hands Furniture",
+			"position" : "Warehouse Manager",
+			"dates" : "September 2013 - Present",
+			"description" : [
+				"Oversee each day to ensure business runs as smoothly as possible for both customers and employees",
+				"Supervise an average of 4 full-time employees plus frequent temp workers",
+				"Fill in as Manager on Duty when needed",
+				"Assist in creating weekly schedules for warehouse employees",
+				"Ensure temporary employees’ time clock punches are accurate and accounted for",
+				"Create daily reports which analyze different aspects of the business such as how much revenue was earned; those reports are then sent to the Chiefs and Directors of the Company",
+				"Solve any issues guests may have face to face or over the phone",
+				"Coordinate multiple incoming shipments on a weekly basis",
+				"Conduct interviews, one on ones (feedback, write ups, verbal warnings, etc.) hire and terminate employees",
+				"Facilitate training on various topics such as warehouse organization, efficiency, customer service and new policies",
+				"Create relationships and generate repeat business",
+				"Continue with my Warehouse Associate responsibilities"
+			]
+		},
+		{
+			"employer" : "RoundUp Fabrications",
+			"position" : "Metal Fabricator",
+			"dates" : "April 2013 - September 2013",
+			"description" : [
+				"Read and interpreted blue prints provided by customer to create their desired product",
+				"Catered mostly to oil field & mechanical based industries",
+				"Solved customers’ issues by designing and selling the appropriate solution/product ",
+				"Gained product and technical knowledge",
+				"Generated revenue and attained strategic goals each month"
+			]
+		},
+		{
+			"employer" : "Texas Legislative Services",
+			"position" : "Legislative Correspondent ",
+			"dates" : "December 2012 - April 2013",
+			"description" : [
+				"Summarized committees for the Texas Legislature, recording record votes and documenting hearings",
+				"Recorded bill statuses (Passed, Vetoed, Denied, Signed, etc.)",
+				"Maintained relationships with house representatives & their office personnel to obtain detailed contribution information and relevant local calendar events"
+			]
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Sample Project 1",
+			"dates" : "2015",
+			"description" : "Sample Bla Bla Bla",
+			"images" : [
+				"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0CAcQjRxqFQoTCOur6eyj18cCFcmQDQodL2QMgg&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dde.lotumapps.vibes&psig=AFQjCNFZ-ngxOQJ_UoS-QHvl-CSAeEj1WA&ust=1441246525451090",
+				"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0CAcQjRxqFQoTCOur6eyj18cCFcmQDQodL2QMgg&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dde.lotumapps.vibes&psig=AFQjCNFZ-ngxOQJ_UoS-QHvl-CSAeEj1WA&ust=1441246525451090"
+			]
+		},
+		{
+			"title" : "Sample Project 2",
+			"dates" : "2015",
+			"description" : "Sample 2 Bla Bla Bla",
+			"images" : [
+				"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0CAcQjRxqFQoTCOur6eyj18cCFcmQDQodL2QMgg&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dde.lotumapps.vibes&psig=AFQjCNFZ-ngxOQJ_UoS-QHvl-CSAeEj1WA&ust=1441246525451090",
+				"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0CAcQjRxqFQoTCOur6eyj18cCFcmQDQodL2QMgg&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dde.lotumapps.vibes&psig=AFQjCNFZ-ngxOQJ_UoS-QHvl-CSAeEj1WA&ust=1441246525451090"
+			]
+		}
+	]
+}
+
+
+//var formattedName = HTMLheaderName.replace("%data%", bio.name);
+//var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//var formattedEmail = HTMLemail.replace("%data%", bio.contacts[0]);
+//var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[1]);
+//var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+//var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+//for (var i = skills.length - 1; i >= 0; i--) {
+//	var formattedskills = HTMLskills.replace("%data%", skills[i]);
+//	$("#header").prepend(formattedskills);
+//};
+//$("#header").prepend(HTMLskillsStart);
+//$("#header").prepend(formattedWelcomeMessage);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//$("#header").prepend(formattedBioPic);
+//$("#topContacts").append(formattedEmail);
+//$("#topContacts").append(formattedMobile);
+
+
+
